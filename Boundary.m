2da2d3784518be676a3db37085d452b8501c5912
Kai He <100274441+e-2718@users.noreply.github.com>
2024-02-01 22:27:31 +0800

    Add files via upload

--- Boundary.m
+++ Boundary.m
@@ -1,4 +1,4 @@
-function zz=Boundary(r0,R,name)
+function z=Boundary(x,R,name)
 
 if nargin==1
     R='C';
@@ -10,7 +10,7 @@ if R=='C';
         name='Boundary_data1.mat';
     end
     load(name)
-    zz=interp1(r',z.',r0,'linear')-H*i;
+    z=interp1(r',z.',x,'linear')-H*i;
 
 elseif R=='P';
     
@@ -18,24 +18,11 @@ elseif R=='P';
         name='Boundary_data2.mat';
     end
     load(name)
-    zz=interp1(al.',z.',r0,'linear')-H*i;
+    z=interp1(al.',z.',x,'linear')-H*i;
     
 end
-% 
-% 
-% if nargin==1
-%     name='Boundary_data1.mat';
-% end
-% 
-% load(name)
-% zz=interp1(r',z.',r0,'linear')-H*i;
 
 end
-
-% plot(al.','b.-')
-% plot(r0,'r.-')
-
-% plot(abs(zz(2:end)-z(1:end-1)),'b.-')
 
 
 
